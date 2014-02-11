d6e4382c0e31cf07c0141af572976c2464dc74b4
vlefevre <vlefevre@280ebfd0-de03-0410-8827-d642c229c3f4>
2014-02-11 12:49:31 +0000

    Created set_exp branch for clean-up of mpfr_set_exp behavior. Note: patches for bugs found via this change should have their own commits in the trunk (so that they can be backported more easily).

---
+++ branches/set_exp/src/check.c
@@ -0,0 +1,81 @@
+/* mpfr_check -- Check if a floating-point number has not been corrupted.
+
+Copyright 2003-2004, 2006-2014 Free Software Foundation, Inc.
+Contributed by the AriC and Caramel projects, INRIA.
+
+This file is part of the GNU MPFR Library.
+
+The GNU MPFR Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+
+The GNU MPFR Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the GNU MPFR Library; see the file COPYING.LESSER.  If not, see
+http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
+51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA. */
+
+#include "mpfr-impl.h"
+
+/*
+ * Check if x is a valid mpfr_t initializes by mpfr_init
+ * Returns 0 if isn't valid
+ */
+int
+mpfr_check (mpfr_srcptr x)
+{
+  mp_size_t s, i;
+  mp_limb_t tmp;
+  volatile mp_limb_t *xm;
+  mpfr_prec_t prec;
+  int rw;
+
+  /* Check sign */
+  if (MPFR_SIGN(x) != MPFR_SIGN_POS &&
+      MPFR_SIGN(x) != MPFR_SIGN_NEG)
+    return 0;
+  /* Check precision */
+  prec = MPFR_PREC(x);
+  if (prec < MPFR_PREC_MIN ||
+      prec > MPFR_PREC_MAX)
+    return 0;
+  /* Check mantissa */
+  xm = MPFR_MANT(x);
+  if (xm == NULL)
+    return 0;
+  /* Check size of mantissa */
+  s = MPFR_GET_ALLOC_SIZE(x);
+  if (s <= 0 || s > MP_SIZE_T_MAX ||
+      prec > (mpfr_prec_t) s * GMP_NUMB_BITS)
+    return 0;
+  /* Acces all the mp_limb of the mantissa: may do a seg fault */
+  for (i = 0 ; i < s ; i++)
+    tmp = xm[i];
+  /* Check singular numbers (do not use MPFR_IS_PURE_FP() in order to avoid
+     any assertion checking, as this function mpfr_check() does something
+     similar by returning a Boolean instead of doing an abort if the format
+     is incorrect). */
+  if (MPFR_IS_SINGULAR (x))
+    return MPFR_IS_ZERO(x) || MPFR_IS_NAN(x) || MPFR_IS_INF(x);
+  /* Check the most significant limb (its MSB must be 1) */
+  if ((xm[MPFR_LAST_LIMB(x)] & MPFR_LIMB_HIGHBIT) == 0)
+    return 0;
+  /* Check the least significant limb (the trailing bits must be 0) */
+  rw = prec % GMP_NUMB_BITS;
+  if (rw != 0)
+    {
+      tmp = MPFR_LIMB_MASK (GMP_NUMB_BITS - rw);
+      if ((xm[0] & tmp) != 0)
+        return 0;
+    }
+  /* Check exponent range */
+  if (MPFR_EXP (x) < __gmpfr_emin ||
+      MPFR_EXP (x) > __gmpfr_emax)
+    return 0;
+  return 1;
+}
---
+++ branches/set_exp/src/random_deviate.h
@@ -0,0 +1,79 @@
+/* Declarations of random_deviate routines for mpfr_erandom and mpfr_nrandom.
+
+Copyright 2013-2014 Free Software Foundation, Inc.
+Contributed by Charles Karney <charles@karney.com>, SRI International.
+
+This file is part of the GNU MPFR Library.
+
+The GNU MPFR Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+
+The GNU MPFR Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the GNU MPFR Library; see the file COPYING.LESSER.  If not, see
+http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
+51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA. */
+
+#if !defined(MPFR_RANDOM_DEVIATE_H)
+#define MPFR_RANDOM_DEVIATE_H 1
+
+#include "mpfr-impl.h"
+
+/* This should be an unsigned type with a width of at least 32 and capable of
+ * representing at least 2*MPFR_PREC_MAX.  This is used to count the bits in
+ * the fraction of a mpfr_random_deviate_t.  See the checks made on this type
+ * in random_deviate_generate. */
+typedef unsigned long mpfr_random_size_t;
+
+typedef struct {
+  mpfr_random_size_t e;       /* total number of bits in the fraction */
+  unsigned long h;            /* the high W bits of the fraction */
+  mpz_t f;                    /* the rest of the fraction */
+} __mpfr_random_deviate_struct;
+
+typedef __mpfr_random_deviate_struct mpfr_random_deviate_t[1];
+typedef __mpfr_random_deviate_struct *mpfr_random_deviate_ptr;
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+  /* allocate and set to (0,1) */
+  void mpfr_random_deviate_init(mpfr_random_deviate_ptr x);
+
+  /* reset to (0,1) */
+  void mpfr_random_deviate_reset(mpfr_random_deviate_ptr x);
+
+  /* deallocate */
+  void mpfr_random_deviate_clear(mpfr_random_deviate_ptr x);
+
+  /* swap two random deviates */
+  void mpfr_random_deviate_swap(mpfr_random_deviate_ptr x,
+                                mpfr_random_deviate_ptr y);
+
+  /* return kth bit of fraction, representing 2^-k */
+  int mpfr_random_deviate_tstbit(mpfr_random_deviate_ptr x,
+                                 mpfr_random_size_t k,
+                                 gmp_randstate_t r);
+
+  /* compare two random deviates, x < y */
+  int mpfr_random_deviate_less(mpfr_random_deviate_ptr x,
+                               mpfr_random_deviate_ptr y,
+                               gmp_randstate_t r);
+
+  /* set mpfr_t z = (neg ? -1 : 1) * (n + x) */
+  int mpfr_random_deviate_value(int neg, unsigned long n,
+                                mpfr_random_deviate_ptr x, mpfr_t z,
+                                gmp_randstate_t r, mpfr_rnd_t rnd);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif
