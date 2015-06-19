2638a5d67609e49e5893b42a425ea5b86651e57c
vlefevre <vlefevre@280ebfd0-de03-0410-8827-d642c229c3f4>
2015-06-19 22:37:44 +0000

    MPFR 3.1.3 tag.

---
+++ tags/3.1.3/autogen.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# "autoreconf -f" will clobber our INSTALL file with a generic one if we
+# don't move it out of the way
+
+mv -f INSTALL INSTALL.$$.tmp
+
+autoreconf -v -f -i -W all
+status=$?
+
+rm -f INSTALL
+mv -f INSTALL.$$.tmp INSTALL
+
+rm -rf autom4te.cache
+
+exit $status
