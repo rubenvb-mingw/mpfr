c819d3e03ef031e860fe84c11fabf1012af1f254
zimmerma <zimmerma@280ebfd0-de03-0410-8827-d642c229c3f4>
2015-07-17 19:15:18 +0000

    use arb_version

--- misc/www/mpfr-3.1.2/timings-arb.c
+++ misc/www/mpfr-3.1.2/timings-arb.c
@@ -64,7 +64,7 @@ main (int argc, char *argv[])
       exit(1);
     }
 
-  printf ("Using ARB-%s with GMP-%s\n", "2.3.0", gmp_version);
+  printf ("Using ARB-%s with GMP-%s\n", arb_version, gmp_version);
   n = atoi(argv[1]);
   prec = (int) ( n * log(10.0) / log(2.0) + 1.0 );
   printf("[precision is %u bits]\n", prec);
