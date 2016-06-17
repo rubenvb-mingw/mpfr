d2eac10c844359c913d8511f2c48c433f142adc0
vlefevre <vlefevre@280ebfd0-de03-0410-8827-d642c229c3f4>
2016-06-17 13:36:48 +0000

    [sum-timings/sum-timings.c] Minor improvements.

--- misc/sum-timings/sum-timings.c
+++ misc/sum-timings/sum-timings.c
@@ -6,11 +6,21 @@
 #include <mpfr.h>
 
 /* Warning! These timings are very inaccurate: in practice, one can get
- * more than a factor 2 (but less than 3) between two invocations. Using
+ * more than a factor 3 (but less than 4) between two invocations. Using
  * only one core improves a bit, but not much (e.g. 0.1192 -> 0.2324 s).
  * However, this test can be regarded as sufficient to compare the three
  * algorithms (sum_old, sum_new, sum_add), because the goal is to detect
  * factors between them much larger than 2.
+ */
+
+/* Minor improvements done on 2016-06-17:
+ *   - Use a single MPFR object for the result. Using one per function
+ *     was useless and could introduce a (probably very little) bias
+ *     due to different alignment and locality.
+ *   - Before doing timings, run each code once, in case the initial run
+ *     would take much more time (this is again unlikely).
+ * However, these improvements will remain unnoticeable until there is a
+ * way to get accurate timings.
  */
 
 /* Note: It may be useful to check with precy slightly different from precx
@@ -40,7 +50,7 @@ check_random (mpfr_rnd_t r, long n, long nc,
               mpfr_prec_t precx, mpfr_prec_t precy,
               unsigned long emax, long ntests, unsigned long seed)
 {
-  mpfr_t *x, s[N];
+  mpfr_t *x, s;
   mpfr_ptr *p;
   long i, j;
   gmp_randstate_t state;
@@ -49,8 +59,7 @@ check_random (mpfr_rnd_t r, long n, long nc,
   gmp_randinit_default (state);
   gmp_randseed_ui (state, seed);
 
-  for (i = 0; i < N; i++)
-    mpfr_init2 (s[i], precy);
+  mpfr_init2 (s, precy);
 
   x = (mpfr_t *) malloc (n * sizeof (mpfr_t));
   p = (mpfr_ptr *) malloc (n * sizeof (mpfr_ptr));
@@ -89,22 +98,25 @@ check_random (mpfr_rnd_t r, long n, long nc,
         mpfr_swap (x[i1], x[i2]);
       }
 
+  /* Before doing timings, run each code once. */
+  for (i = 0; i < N; i++)
+    fp[i] (s, p, n, r);
+
   for (i = 0; i < N; i++)
     {
       c = clock ();
       for (j = 0; j < ntests; j++)
-        fp[i] (s[i], p, n, r);
+        fp[i] (s, p, n, r);
       c = clock () - c;
       mpfr_printf ("%s took %7.4f s  (%Rg)\n", fn[i],
-                   (double) c / CLOCKS_PER_SEC, s[i]);
+                   (double) c / CLOCKS_PER_SEC, s);
     }
 
   for (i = 0; i < n; i++)
     mpfr_clear (x[i]);
   free (x);
   free (p);
-  for (i = 0; i < N; i++)
-    mpfr_clear (s[i]);
+  mpfr_clear (s);
   gmp_randclear (state);
 }
 
